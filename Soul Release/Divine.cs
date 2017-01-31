346c08a691b856925986fcb5cbe30baba2edb332
ichiro2fly85 <ichiro2fly85@gmail.com>
2017-01-30 21:28:06 -0500

    New folder for SR

---
+++ Soul Release/Divine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Base script to the main character Divine for the game Soul Release
+/// </summary>
+public class Divine
+{
+	public Divine()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
